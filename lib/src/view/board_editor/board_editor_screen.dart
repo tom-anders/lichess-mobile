8cfd4cc70b3e07b0cef873f2ed1fd68b325313a9
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-07-17 17:15:23 +0200

    make menu a bit nicer

--- lib/src/view/board_editor/board_editor_screen.dart
+++ lib/src/view/board_editor/board_editor_screen.dart
@@ -1,18 +1,19 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:chessground/chessground.dart' as cg;
 import 'package:dartchess/dartchess.dart' as dc;
-import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lichess_mobile/src/constants.dart';
+import 'package:lichess_mobile/src/model/analysis/analysis_controller.dart';
+import 'package:lichess_mobile/src/model/common/chess.dart';
 import 'package:lichess_mobile/src/model/settings/board_preferences.dart';
 import 'package:lichess_mobile/src/styles/lichess_icons.dart';
 import 'package:lichess_mobile/src/styles/styles.dart';
 import 'package:lichess_mobile/src/utils/l10n_context.dart';
+import 'package:lichess_mobile/src/utils/navigation.dart';
 import 'package:lichess_mobile/src/utils/screen.dart';
-import 'package:lichess_mobile/src/widgets/adaptive_text_field.dart';
+import 'package:lichess_mobile/src/view/analysis/analysis_screen.dart';
 import 'package:lichess_mobile/src/widgets/bottom_bar_button.dart';
-import 'package:lichess_mobile/src/widgets/buttons.dart';
 import 'package:lichess_mobile/src/widgets/platform.dart';
 
 class BoardEditorScreen extends StatelessWidget {
@@ -55,8 +56,30 @@ class _Body extends StatefulWidget {
 }
 
 class _BodyState extends State<_Body> {
+  cg.Side orientation = cg.Side.white;
+
+  cg.Pieces pieces = cg.readFen(dc.kInitialBoardFEN);
+
+  dc.Chess? position;
+
   @override
   Widget build(BuildContext context) {
+    final fen = cg.writeFen(pieces);
+    final board = dc.Board.parseFen(fen);
+    try {
+      position = dc.Chess.fromSetup(
+        dc.Setup(
+          board: board,
+          unmovedRooks: dc.SquareSet.corners,
+          turn: dc.Side.white,
+          halfmoves: 0,
+          fullmoves: 1,
+        ),
+      );
+    } catch (_) {
+      position = null;
+    }
+
     return Column(
       children: [
         Expanded(
@@ -85,23 +108,46 @@ class _BodyState extends State<_Body> {
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    _Board(boardSize, isTablet: isTablet),
-                    ColoredBox(
-                      color: Colors.grey,
-                      child: Flex(
-                        direction: direction,
-                        children: [
-                          _PieceMenu(
-                            boardSize,
-                            direction: flipAxis(direction),
-                            side: cg.Side.white,
-                          ),
-                          _PieceMenu(
-                            boardSize,
-                            direction: flipAxis(direction),
-                            side: cg.Side.black,
-                          ),
-                        ],
+                    _BoardEditor(
+                      boardSize,
+                      orientation: orientation,
+                      isTablet: isTablet,
+                      pieces: pieces,
+                      onDiscardedPiece: (squareId) => setState(() {
+                        pieces.remove(squareId);
+                      }),
+                      onDroppedPiece: (origin, destination, piece) =>
+                          setState(() {
+                        pieces[destination] = piece;
+                        if (origin != null) {
+                          pieces.remove(origin);
+                        }
+                      }),
+                    ),
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(4.0)),
+                        boxShadow: boardShadows,
+                      ),
+                      child: ColoredBox(
+                        color: Colors.grey,
+                        child: Flex(
+                          direction: direction,
+                          children: [
+                            _PieceMenu(
+                              boardSize,
+                              direction: flipAxis(direction),
+                              side: orientation.opposite,
+                            ),
+                            _PieceMenu(
+                              boardSize,
+                              direction: flipAxis(direction),
+                              side: orientation,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
@@ -110,43 +156,86 @@ class _BodyState extends State<_Body> {
             ),
           ),
         ),
-        _BottomBar(),
+        _BottomBar(
+          onFlipBoard: () => setState(() {
+            orientation = orientation.opposite;
+          }),
+          onOpenInAnalysis: position != null
+              ? () {
+                  final pgn = dc.PgnGame(
+                    headers: {'Variant': 'from position', 'FEN': position!.fen},
+                    moves: dc.PgnNode<dc.PgnNodeData>(),
+                    comments: [],
+                  );
+                  pushPlatformRoute(
+                    context,
+                    rootNavigator: true,
+                    builder: (context) => AnalysisScreen(
+                      pgnOrId: pgn.makePgn(),
+                      options: AnalysisOptions(
+                        isLocalEvaluationAllowed: true,
+                        variant: Variant.standard,
+                        orientation: (orientation == cg.Side.white)
+                            ? dc.Side.white
+                            : dc.Side.black,
+                        id: standaloneAnalysisId,
+                      ),
+                    ),
+                  );
+                }
+              : null,
+          onContinueFromHere: position != null
+              ? () {
+                  // TODO
+                }
+              : null,
+        ),
       ],
     );
   }
 }
 
-class _Board extends ConsumerStatefulWidget {
-  const _Board(
+class _BoardEditor extends ConsumerWidget {
+  const _BoardEditor(
     this.boardSize, {
     required this.isTablet,
+    required this.orientation,
+    required this.onDroppedPiece,
+    required this.onDiscardedPiece,
+    required this.pieces,
   });
 
   final double boardSize;
   final bool isTablet;
+  final cg.Side orientation;
+  final cg.Pieces pieces;
+
+  final void Function(
+    cg.SquareId? origin,
+    cg.SquareId destination,
+    cg.Piece piece,
+  )? onDroppedPiece;
+  final void Function(cg.SquareId square)? onDiscardedPiece;
 
   @override
-  ConsumerState<_Board> createState() => _BoardState();
-}
-
-class _BoardState extends ConsumerState<_Board> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final boardPrefs = ref.watch(boardPreferencesProvider);
 
     return cg.BoardEditor(
-      size: widget.boardSize,
-      initialFen: dc.kInitialBoardFEN,
-      orientation: cg.Side.white, // TODO allow to flip
+      size: boardSize,
+      pieces: pieces,
+      orientation: orientation,
       settings: cg.BoardEditorSettings(
         pieceAssets: boardPrefs.pieceSet.assets,
         colorScheme: boardPrefs.boardTheme.colors,
         enableCoordinates: boardPrefs.coordinates,
-        borderRadius: widget.isTablet
+        borderRadius: isTablet
             ? const BorderRadius.all(Radius.circular(4.0))
             : BorderRadius.zero,
-        boxShadow: widget.isTablet ? boardShadows : const <BoxShadow>[],
+        boxShadow: isTablet ? boardShadows : const <BoxShadow>[],
       ),
+      onDiscardedPiece: onDiscardedPiece,
+      onDroppedPiece: onDroppedPiece,
     );
   }
 }
@@ -176,18 +265,27 @@ class _PieceMenuState extends ConsumerState<_PieceMenu> {
     return Flex(
       direction: widget.direction,
       mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
       children: cg.Role.values.map(
         (role) {
+          final piece = cg.Piece(role: role, color: widget.side);
+          final size = widget.boardSize / 8;
           final pieceWidget = cg.PieceWidget(
-            piece: cg.Piece(role: role, color: widget.side),
-            size: widget.boardSize / 8,
+            piece: piece,
+            size: size,
             pieceAssets: boardPrefs.pieceSet.assets,
           );
 
-          // TODO double size+offset like in the normal board
           return Draggable(
             data: cg.Piece(role: role, color: widget.side),
-            feedback: pieceWidget,
+            feedback: Transform.translate(
+              offset: const Offset(-0.5, -1.5) * size,
+              child: cg.PieceWidget(
+                piece: piece,
+                size: 2 * size,
+                pieceAssets: boardPrefs.pieceSet.assets,
+              ),
+            ),
             child: pieceWidget,
           );
         },
@@ -197,7 +295,17 @@ class _PieceMenuState extends ConsumerState<_PieceMenu> {
 }
 
 class _BottomBar extends ConsumerWidget {
-  const _BottomBar();
+  const _BottomBar({
+    required this.onFlipBoard,
+    required this.onOpenInAnalysis,
+    required this.onContinueFromHere,
+  });
+
+  final void Function() onFlipBoard;
+
+  final void Function()? onOpenInAnalysis;
+
+  final void Function()? onContinueFromHere;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -222,22 +330,22 @@ class _BottomBar extends ConsumerWidget {
               Expanded(
                 child: BottomBarButton(
                   label: context.l10n.flipBoard,
-                  onTap: () {},
+                  onTap: onFlipBoard,
                   icon: Icons.flip,
                 ),
               ),
               Expanded(
                 child: BottomBarButton(
-                  label: context.l10n.continueFromHere,
-                  onTap: () {},
-                  icon: LichessIcons.crossed_swords,
+                  label: context.l10n.analysis,
+                  onTap: onOpenInAnalysis,
+                  icon: LichessIcons.microscope,
                 ),
               ),
               Expanded(
                 child: BottomBarButton(
-                  label: context.l10n.analysis,
-                  onTap: () {},
-                  icon: LichessIcons.microscope,
+                  label: context.l10n.continueFromHere,
+                  onTap: onContinueFromHere,
+                  icon: LichessIcons.crossed_swords,
                 ),
               ),
               Expanded(
