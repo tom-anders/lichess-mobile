1422d9efca23cd8691ce0ea325b0d5ba27516a59
tom-anders <13141438+tom-anders@users.noreply.github.com>
2025-06-03 22:17:14 +0200

    add foreast logic

--- lib/src/model/analysis/forecast.dart
+++ lib/src/model/analysis/forecast.dart
@@ -1,17 +1,22 @@
-import 'dart:convert';
+// ignore_for_file: slash_for_doc_comments
 
-import 'package:dartchess/dartchess.dart';
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:collection/collection.dart';
 import 'package:deep_pick/deep_pick.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:lichess_mobile/src/model/common/chess.dart';
-import 'package:lichess_mobile/src/model/common/node.dart';
 import 'package:lichess_mobile/src/model/common/uci.dart';
 
 part 'forecast.freezed.dart';
 part 'forecast.g.dart';
 
-/// Logic based on ForecastCtrl from lichobile
+const kMaxForecastPlies = 30;
+
+/// Contains the current set of conditional premoves ("forecast") in a correspondence game.
+///
+/// Most logic is based on ForecastCtrl from lichobile
 @Freezed(fromJson: true, toJson: true)
 sealed class Forecast with _$Forecast {
   const Forecast._();
@@ -25,6 +30,58 @@ sealed class Forecast with _$Forecast {
 
   factory Forecast.fromServerJson(Map<String, dynamic> json) =>
       forecastFromPick(pick(json).required());
+
+  /// Two forecasts are considered to collide if the current player cannot theoretically play both of them.
+  /// For example, for black, 1. e4 e5 and 1. e4 c5 are colliding forecasts, as black cannot play both e5 and c5
+  /// in response to e4. However, for white, 1. e4 e5 and 1. e4 c5 are not colliding forecasts.
+  ///
+  /// If it is the current player's turn, divergences on the first forecasted move are not considered to collide.
+  /// For example, 2. e4 and 2. d4 are not collisions on white's turn.
+  bool _collides(UciPath lhs, UciPath rhs) {
+    for (var i = 0; i < min(lhs.size, rhs.size); i++) {
+      if (lhs[i] != rhs[i]) {
+        if (onMyTurn) {
+          return i != 0 && i.isEven;
+        }
+        return i.isOdd;
+      }
+    }
+
+    // If the two forecast lines are the same up to the size of the smaller one, they are not considered
+    // to be contradictory, but the smaller is considered a prefix of the larger
+    return false;
+  }
+
+  bool _isLongEnough(UciPath path) {
+    return path.size >= (onMyTurn ? 1 : 2);
+  }
+
+  /// Returns the given line forecast steps, truncated to a reasonable length and ending with the viewing player's move.
+  UciPath _truncate(UciPath path) {
+    final requiredPlyMod = onMyTurn ? 1 : 0;
+
+    // must end with player move
+    return (path.size % 2 != requiredPlyMod ? path.penultimate : path).truncate(kMaxForecastPlies);
+  }
+
+  /// Returns whether the given path is part of a new candidate premove line that is not a prefix of the other saved conditional premove lines.
+  bool isCandidate(UciPath path) {
+    final forecastCandidate = _truncate(path);
+    if (!_isLongEnough(forecastCandidate)) {
+      return false;
+    }
+
+    return lines.none((line) => line.contains(forecastCandidate));
+  }
+
+  @useResult
+  Forecast add(UciPath newLine) {
+    return copyWith(
+      lines: lines
+          .removeWhere((line) => newLine.contains(line) || _collides(line, newLine))
+          .add(newLine),
+    );
+  }
 
   // TODO we currently have no mapping from UciCharPair back to an UCI string.
   // But we can use the Node API to get the nodes on a path and get the UCI from that.
